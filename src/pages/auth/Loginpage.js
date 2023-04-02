7af4a515396c6e7868d337e1ed782096aed0045d
Roketskyi Roman <91413976+Roketskyi@users.noreply.github.com>
2023-04-03 01:29:32 +0300

    update

---
+++ src/pages/auth/Loginpage.js
@@ -0,0 +1,136 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from "react-router-dom";
+
+import './loginpage.css';
+
+function Loginpage({isLogin, setIsLogin}) { 
+    const navigate = useNavigate();
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+
+        localStorage.setItem('isLogin', isLogin ? true : false);
+
+        
+    }
+
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
+    const [loginDirty, setLoginDirty] = useState(false);
+    const [passwordDirty, setPasswordDirty] = useState(false);
+    const [loginError, setLoginError] = useState('Login cannot be empty');
+    const [passwordError, setPasswordError] = useState('Password cannot be empty');
+    const [formValid, setFormValid] = useState(false);
+
+    useEffect(() => {
+        if (loginError || passwordError) setFormValid(false);
+        else setFormValid(true);
+    }, [loginError, passwordError]);
+
+    const loginHandler = (e) => {
+        const re = /^[a-zA-Z0-9_.]+$/
+        if (!re.test(e.target.value)) setLoginError('Incorrect login');
+        else if (e.target.value.length < 4 || e.target.value.length > 12) setLoginError('Login must be more than 4 and less than 12 characters');
+        else setLoginError('');
+        
+        setInvalidAuth(false);
+        setLogin(e.target.value);
+    }
+
+    const passwordHandler = (e) => {
+        if (!e.target.value) setPasswordError('Password cannot be empty');
+        else if (e.target.value.length < 4 || e.target.value.length > 10) setPasswordError('Password must be more than 4 and less than 10 characters');
+        else setPasswordError('');
+        
+        setInvalidAuth(false);
+        setPassword(e.target.value);
+    }
+    
+    const blurHandle = (e) => {
+        switch (e.target.name) {
+            case 'login':
+                setLoginDirty(true);
+            break;
+
+            case 'password':
+                setPasswordDirty(true);
+            break;
+                
+            default:
+        }
+    }
+                
+    // check login/password in base
+    const [invalidAuth, setInvalidAuth] = useState(false);
+
+    const authHandler = async () => {    
+        try {
+            const response = await axios.get('/api/find/' + login);
+            const base = response.data[0];
+      
+            if (!base) {
+                setInvalidAuth(true);
+                return;
+            }
+    
+            const responseSubmit = await axios.post("/api/submit", { login, password })
+    
+            if (responseSubmit.status === 200) {
+                setIsLogin(true);
+                setInvalidAuth(false);
+                navigate('/');
+            } else {
+                setInvalidAuth(true);
+            }
+        } catch (error) {
+            console.error(error);
+            setInvalidAuth(true);
+        }
+    };
+    
+    
+    return (
+        <form className="loginForm" onSubmit={handleLogin} method="post">
+            <h1 className="adminPanel">Admin Panel</h1>
+
+            {invalidAuth && <div className="inputsError invalidAuth">Invalid login/password</div>}
+
+            <div>
+                {(loginDirty && loginError) && <div className='inputsError'>{loginError}</div>}
+                <input
+                    onChange={loginHandler}
+                    onBlur={blurHandle}
+                    name="login"
+                    value={login}
+                    className="loginFormInput"
+                    type="text"
+                    placeholder="Login"
+                    autoComplete="username"
+                    required                
+                />
+            </div>
+
+            {(passwordDirty && passwordError) && <div className='inputsError'>{passwordError}</div>}
+            <div>
+                <input
+                    onChange={passwordHandler}
+                    onBlur={blurHandle}
+                    name="password"
+                    value={password}
+                    className="loginFormInput"
+                    type="password"
+                    placeholder="Password"
+                    autoComplete="current-password"
+                    required                
+                    />
+            </div>
+
+            <div>
+                <button className='btnLogin' disabled={!formValid} type="submit" onClick={authHandler}>Login</button>
+            </div>
+        </form>
+    )
+}
+
+export {Loginpage}
